60f9f68555cd679d518192b0c995da82d1b52569
Shyam Sudhakaran <shyamsnair@protonmail.com>
2020-11-26 00:08:22 -0800

    fixed init

--- yolov3/__init__.py
+++ yolov3/__init__.py
@@ -1,9 +1,9 @@
-from .darknet import Darknet
-from .inference import (
+from yolov3.darknet import Darknet
+from yolov3.inference import (
     cxywh_to_tlbr, draw_boxes, non_max_suppression, inference, to_coco,
     detect_in_cam, detect_in_video
 )
-from . import devtools
+from yolov3 import devtools
 from yolov3.yolo_runner import YoloRunner
 
 __all__ = [
